27d2663a359cd1a156144509576dd4485fd1fc5d
Vincent Beilke <vincent.beilke@gmail.com>
2021-03-03 11:11:00 -0600

    Movie Controller and Models
    - Added MovieCatalog.Models reference
    - Added MoviesViewModel.cs file
    - Added MoviesController.cs file

---
+++ MediaOrganizer/Controllers/MoviesController.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using MediaCatalog.Models;
+using MediaOrganizer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaOrganizer.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private static string BASEURL = "https://localhost:5001/";
+
+        public async Task<ActionResult> List()
+        {
+            var moviesViewModel = new MoviesViewModel();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASEURL);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync("api/Movies");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var movieResponse = response.Content.ReadAsStringAsync().Result;
+
+                    moviesViewModel.Movies = JsonConvert.DeserializeObject<List<MovieModel>>(movieResponse);
+                }
+            }
+
+            return View(moviesViewModel);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            var movie = new MovieModel();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASEURL);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/movies/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var movieResponse = response.Content.ReadAsStringAsync().Result;
+
+                    movie = JsonConvert.DeserializeObject<MovieModel>(movieResponse);
+                }
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddMovie(MovieModel movie)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri($"{BASEURL}/api/movie");
+
+                var result = await client.PostAsJsonAsync<MovieModel>("movie", movie);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("List");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(movie);
+        }
+    }
+}
---
+++ MediaOrganizer/ViewModels/MoviesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaCatalog.Models;
+
+namespace MediaOrganizer.ViewModels
+{
+    public class MoviesViewModel
+    {
+        public IEnumerable<MovieModel> Movies { get; set; }
+        public string Category { get; set; }
+    }
+}
